ee1f8415544a3c2ebde7bc90616db0f6c0774252
yqrashawn <namy.19@gmail.com>
2017-08-20 16:07:59 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+const test = require('./lib/test');
+const log = require('./lib/output');
+const config = require(path.join(process.cwd(), 'puppeteering.js'));
+
+const testUrl = config.url;
+const testPort = config.port;
+const headless = config.headless;
+const timeOut = config.timeOut;
+const https = config.https ? 'https' : 'http';
+
+async function run(beforeLoad, afterLoad) {
+  const browser = await puppeteer.launch({ headless, });
+  const page = await browser.newPage();
+  page.on('pageerror',(...args) => {
+    log.error(...args);
+  });
+  beforeLoad(browser, page);
+  console.log('launching ', `${https}://${testUrl}:${testPort}`);
+  await page.goto(`${https}://${testUrl}:${testPort}`, { waitUntil: 'networkidle' });
+  afterLoad(browser, page);
+  page.waitFor(timeOut).then(() => {
+    browser.close();
+  });
+}
+
+module.export = {
+  run,
+  test,
+};
