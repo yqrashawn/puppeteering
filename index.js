635915d71c486350c1d45747d4fd9a6c1778452c
yqrashawn <namy.19@gmail.com>
2017-08-20 17:38:46 +0800

    version

--- index.js
+++ index.js
@@ -2,32 +2,9 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 const test = require('./lib/test.js');
 const log = require('./lib/output.js');
-const configpath = path.join(process.cwd(), 'puppeteering.js');
-const config = require(configpath);
-
-const testUrl = config.url;
-const testPort = config.port;
-const headless = config.headless;
-const timeout = config.timeout;
-const https = config.https ? 'https' : 'http';
-
-async function run(beforeLoad, afterLoad) {
-  const browser = await puppeteer.launch({ headless, });
-  const page = await browser.newPage();
-  page.on('pageerror', (...args) => {
-    log.error(...args);
-  });
-  beforeLoad(browser, page);
-  console.log('launching ', `${https}://${testUrl}:${testPort}`);
-  await page.goto(`${https}://${testUrl}:${testPort}`, { waitUntil: 'networkidle' });
-  afterLoad(browser, page);
-  page.waitFor(timeout).then(() => {
-    browser.close();
-  });
-}
 
 module.exports = {
-  run,
+  puppeteer,
   error: log.error,
   test,
 };
