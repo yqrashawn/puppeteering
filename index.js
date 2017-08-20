4b4b14a5b03407e6a7f6435ed19f8ad39a5f53d0
yqrashawn <namy.19@gmail.com>
2017-08-20 17:40:49 +0800

    version

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const puppeteer = require('puppeteer');
-const path = require('path');
 const test = require('./lib/test.js');
 const log = require('./lib/output.js');
 
 module.exports = {
-  puppeteer,
   error: log.error,
   test,
 };
